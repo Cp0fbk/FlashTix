47e7d6c38e676e7de85b6f0decee2c767aca357e
Chau Phuc <chauphuc09090@gmail.com>
2025-12-24 21:07:13 +0700

    feat: load env

--- backend/src/main/java/com/flashtix/FlashtixApplication.java
+++ backend/src/main/java/com/flashtix/FlashtixApplication.java
@@ -1,13 +1,24 @@
 package com.flashtix;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlashtixApplication {
+    private static Dotenv dotenv;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlashtixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .ignoreIfMissing()
+                .systemProperties()
+                .load();
+        SpringApplication.run(FlashtixApplication.class, args);
+    }
 
+    public static Dotenv getEnv() {
+        // Implementation for loading .env file if needed
+        return dotenv;
+    }
 }
